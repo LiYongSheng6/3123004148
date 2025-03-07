017774ad3ca49326f38b08fb1a704d3f7972410a
LiYongSheng6 <lys3229696977@163.com>
2025-03-08 00:35:47 +0800

    <refactor>主函数逻辑重构

--- src/main/java/com/example/PlagiarismChecker.java
+++ src/main/java/com/example/PlagiarismChecker.java
@@ -16,47 +16,23 @@ public class PlagiarismChecker {
      * 命令行参数，依次为：原文文件路径、抄袭版文件路径、答案文件路径
      */
     public static void main(String[] args) throws IOException {
-        // 创建一个StringBuilder用于存储查重结果
-        StringBuilder stringBuilder = new StringBuilder();
-
-        // 存储原文文件路径和输出文件路径
-        String originalFilePath, outputFilePath;
-
         // 检查命令行参数数量是否正确
         if (args.length != 3) {
-            //System.err.println("Usage: java -jar main.jar <originalFile> <plagiarizedFile> <answerFile>");
-            //System.exit(1);
-
-            // 存储原文文件路径
-            originalFilePath = "D:\\code\\Java\\PaperPlagiarismCheck\\src\\main\\resources\\orig.txt";
-            // 存储输出文件路径
-            outputFilePath = "D:\\code\\Java\\PaperPlagiarismCheck\\src\\main\\resources\\ans.txt";
-            // 存储抄袭版文件路径
-            String[] plagiarizedFilePaths = {
-                    "D:\\code\\Java\\PaperPlagiarismCheck\\src\\main\\resources\\orig_0.8_add.txt",
-                    "D:\\code\\Java\\PaperPlagiarismCheck\\src\\main\\resources\\orig_0.8_del.txt",
-                    "D:\\code\\Java\\PaperPlagiarismCheck\\src\\main\\resources\\orig_0.8_dis_1.txt",
-                    "D:\\code\\Java\\PaperPlagiarismCheck\\src\\main\\resources\\orig_0.8_dis_10.txt",
-                    "D:\\code\\Java\\PaperPlagiarismCheck\\src\\main\\resources\\orig_0.8_dis_15.txt",
-            };
-
-            // 遍历抄袭版文件路径，依次查重处理
-            for (String plagiarizedFilePath : plagiarizedFilePaths) {
-                checkSimilarity(plagiarizedFilePath, originalFilePath, stringBuilder);
-            }
+            System.err.println("Usage: java -jar main.jar <originalFile> <plagiarizedFile> <answerFile>");
+            System.exit(1);
         } else {
+            // 创建一个StringBuilder用于存储查重结果
+            StringBuilder stringBuilder = new StringBuilder();
             // 从命令行参数中获取文件路径
-            originalFilePath = args[0];
-            outputFilePath = args[2];
+            String originalFilePath = args[0];
+            String outputFilePath = args[2];
             String plagiarizedFilePath = args[1];
-
             // 查重处理
             checkSimilarity(plagiarizedFilePath, originalFilePath, stringBuilder);
+            // 将结果写入答案文件
+            writeFileWithBuffer(outputFilePath, stringBuilder.toString());
+            System.out.println("Result written to " + outputFilePath);
         }
-
-        // 将结果写入答案文件
-        writeFileWithBuffer(outputFilePath, stringBuilder.toString());
-        System.out.println("Result written to " + outputFilePath);
     }
 
 }
